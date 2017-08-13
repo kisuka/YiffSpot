c1b1fa4f85112bc94c9aaf1a70b18816ae76a31c
Taylor Locke <kisuka@kisuka.com>
2017-08-13 03:53:29 -0700

    Correction to heatbeat

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express    = require('express')
 var app        = express();
 var server     = require('http').Server(app);
-var io         = require('socket.io')(server, {'pingInterval': 60000, 'pingTimeout': 30000});
+var io         = require('socket.io')(server, {'pingInterval': 25000, 'pingTimeout': 60000});
 var connection = require('./src/server/connection.js');
 
 var port = process.env.PORT || 3000;
