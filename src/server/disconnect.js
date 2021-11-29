cb03c1eef13b75e67e087557307a1b3951ef8cdc
xhayper <hayper1919@gmail.com>
2021-11-29 18:06:41 +0700

    Update disconnect.js

--- src/server/disconnect.js
+++ src/server/disconnect.js
@@ -9,6 +9,8 @@ module.exports = (users, token) => {
 
   // Check if user has a partner
   if (partner) {
+    currentUser.previousPartner = currentUser.partner;
+    
     // Disconnect user from partner.
     users.removePartner(currentUser.id);
 
