1b908a892d33d6ecaa7d9da17d833458a1225027
xhayper <hayper1919@gmail.com>
2021-11-29 18:33:48 +0700

    ;w;

--- src/server/init.js
+++ src/server/init.js
@@ -83,7 +83,7 @@ module.exports = wss => {
               return;
             }
 
-            if (clients[partner.id] && clients[partner.id].partner && partner.socket.readyState == 1) {
+            if (users[partner.id] && users[partner.id].partner && partner.socket.readyState == 1) {
               partner.socket.send(JSON.stringify({ type: 'partner_left', data: true }));
             }
             
